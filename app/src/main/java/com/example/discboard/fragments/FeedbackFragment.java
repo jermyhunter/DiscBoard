e4176df08be65932b8bc4dd42a37d17eb1a9d0c6
yugar <32658903+jermyhunter@users.noreply.github.com>
2023-04-10 20:22:25 +0800

    add QR code picture

--- app/src/main/java/com/example/discboard/fragments/FeedbackFragment.java
+++ app/src/main/java/com/example/discboard/fragments/FeedbackFragment.java
@@ -5,17 +5,32 @@ import static com.example.discboard.DiscFinal.*;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.discboard.BuildConfig;
 import com.example.discboard.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +41,8 @@ public class FeedbackFragment extends Fragment {
     public FeedbackFragment() {
         // Required empty public constructor
     }
+
+    ImageView mQRCodeImg;
 
     /**
      * Use this factory method to create a new instance of
@@ -62,14 +79,65 @@ public class FeedbackFragment extends Fragment {
         });
 
         // 复制链接到剪切板
-        v.findViewById(R.id.copy_url_btn).setOnClickListener(new View.OnClickListener() {
+        v.findViewById(R.id.visit_homepage_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("", CONTACT_URL);
-                clipboard.setPrimaryClip(clip);
+                // clipboard
+//                ClipboardManager clipboard = (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+//                ClipData clip = ClipData.newPlainText("", CONTACT_URL);
+//                clipboard.setPrimaryClip(clip);
+
+                try {
+                    Intent i = new Intent();
+                    i.putExtra(Intent.EXTRA_TEXT, CONTACT_URL);
+                    i.setAction(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(CONTACT_URL));
+                    startActivity(i);
+                } catch (Exception e) {
+                    Log.e("In Exception", "Comes here");
+                    Intent i = new Intent();
+                    i.putExtra(Intent.EXTRA_TEXT, CONTACT_URL);
+                    i.setAction(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(CONTACT_URL));
+                    startActivity(i);
+                }
             }
         });
+
+        mQRCodeImg = v.findViewById(R.id.qr_code_img);
+        mQRCodeImg.post(() -> {
+            //resize
+            int boardWidth = mQRCodeImg.getWidth();
+            int boardHeight = mQRCodeImg.getHeight();
+            ViewGroup.LayoutParams lp = mQRCodeImg.getLayoutParams();
+//            float ratio = (float)(boardWidth - 20) / boardWidth;
+            float ratio = 4 / 5f;
+            lp.width = (int) (boardWidth * ratio);
+            lp.height = (int) (boardHeight * ratio);
+            mQRCodeImg.setLayoutParams(lp);
+            mQRCodeImg.setVisibility(View.VISIBLE);
+        });
+
+        Animation fadeInAnim = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
+        fadeInAnim.setStartOffset(2 * 1000);
+        fadeInAnim.setDuration(2 * 1000);
+        fadeInAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mQRCodeImg.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+//        mQRCodeImg.startAnimation(fadeInAnim);
 
         String version_name = BuildConfig.VERSION_NAME;
         TextView verInfoTxt = v.findViewById(R.id.ver_info_txt);
